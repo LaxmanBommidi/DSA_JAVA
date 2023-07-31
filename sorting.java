26b773c5c10d3c652ac1ec1ef2df946ea4583ce4
laxman Bommidi <bommidilaxman1017@gmail.com>
2023-07-31 22:41:51 +0530

    Soring is done

---
+++ sorting.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class sorting
+{
+    public static void main(String args[])
+    {
+        int arr[] = {5,4,3,2,1};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
